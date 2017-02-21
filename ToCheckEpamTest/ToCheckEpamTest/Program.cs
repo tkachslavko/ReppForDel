94e8166c147adf95dd1634674142c7cb52998c68
Yaroslav Tkach <tkachslavko@gmail.com>
2017-02-21 18:09:34 +0200

    Add one coment

---
+++ ToCheckEpamTest/ToCheckEpamTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+//Now I wil try to add anotetion to this project
+namespace ToCheckEpamTest
+{
+    class Program
+    {
+        delegate int MyDelegate(int time);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate=FuncForDelegate;
+            myDelegate.BeginInvoke( 5000, EndOfDelegateWork, null);
+            for (int i = 0; i < 5000; i+=100)
+            {
+                Console.Write(".");
+                Thread.Sleep(50);
+            }
+            Console.WriteLine("Current Thread finished!");
+            Console.ReadLine();
+        }
+        public static int FuncForDelegate(int i)
+        {
+            for (int k = 0; k < i; k+=100)
+            {
+                Console.Write(k);
+                Thread.Sleep(100);
+            }   
+            return i * i;
+        }
+        public static void EndOfDelegateWork(IAsyncResult iar)
+        {
+            Console.WriteLine("End of delegate work...");
+        }
+    }
+}
